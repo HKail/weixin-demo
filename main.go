a9fad8a6abe3653e6a329a7bae8c26ee075a191f
Allen <934932687@qq.com>
2020-04-04 16:04:56 +0800

    complete message reply

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"time"
 	"weixin-demo/util"
 )
 
@@ -54,4 +57,35 @@ func WXMsgReceive(c *gin.Context) {
 	}
 
 	log.Printf("[消息接收] - 收到消息, 消息类型为: %s, 消息内容为: %s\n", textMsg.MsgType, textMsg.Content)
+
+	WXMsgReply(c, textMsg.ToUserName, textMsg.FromUserName)
+}
+
+// WXRepTextMsg 微信回复文本消息结构体
+type WXRepTextMsg struct {
+	ToUserName   string
+	FromUserName string
+	CreateTime   int64
+	MsgType      string
+	Content      string
+	// 若不标记XMLName, 则解析后的xml名为该结构体的名称
+	XMLName xml.Name `xml:"xml"`
+}
+
+// WXMsgReply 微信消息回复
+func WXMsgReply(c *gin.Context, fromUser, toUser string) {
+	repTextMsg := WXRepTextMsg{
+		ToUserName:   fromUser,
+		FromUserName: toUser,
+		CreateTime:   time.Now().Unix(),
+		MsgType:      "text",
+		Content:      fmt.Sprintf("[消息回复] - %s", time.Now().Format("2006-01-02 15:04:05")),
+	}
+
+	msg, err := xml.Marshal(&repTextMsg)
+	if err != nil {
+		log.Printf("[消息回复] - 将对象进行XML编码出错: %v\n", err)
+		return
+	}
+	_, _ = c.Writer.Write(msg)
 }

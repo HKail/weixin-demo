47dbe2e5875c2af5b881e93266d4a29a974dff27
Allen <934932687@qq.com>
2020-04-04 14:39:29 +0800

    weixin check signature

--- main.go
+++ main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"weixin-demo/util"
 )
+
+const Token = "coleliedev"
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"msg": "Hello world!"})
-	})
+	router.GET("/", WXCheckSignature)
 
 	log.Fatalln(router.Run(":80"))
+}
+
+// WXCheckSignature 微信接入校验
+func WXCheckSignature(c *gin.Context) {
+	signature := c.Query("signature")
+	timestamp := c.Query("timestamp")
+	nonce := c.Query("nonce")
+	echostr := c.Query("echostr")
+
+	ok := util.CheckSignature(signature, timestamp, nonce, Token)
+	if !ok {
+		log.Println("微信公众号接入校验失败!")
+		return
+	}
+
+	log.Println("微信公众号接入校验成功!")
+	_, _ = c.Writer.WriteString(echostr)
 }
---
+++ util/util.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"sort"
+	"strings"
+)
+
+// CheckSignature 微信公众号签名检查
+func CheckSignature(signature, timestamp, nonce, token string) bool {
+	arr := []string{timestamp, nonce, token}
+	// 字典序排序
+	sort.Strings(arr)
+
+	n := len(timestamp) + len(nonce) + len(token)
+	var b strings.Builder
+	b.Grow(n)
+	for i := 0; i < len(arr); i++ {
+		b.WriteString(arr[i])
+	}
+
+	return Sha1(b.String()) == signature
+}
+
+// 进行Sha1编码
+func Sha1(str string) string {
+	h := sha1.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
